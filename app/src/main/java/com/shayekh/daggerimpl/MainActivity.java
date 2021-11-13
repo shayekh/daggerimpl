6b5a0fad49884209ee1f7bbebd6eed8fc7924f18
shayekh <shayekhebnemizan@gmail.com>
2021-11-13 15:38:22 +0600

    Till 5

--- app/src/main/java/com/shayekh/daggerimpl/MainActivity.java
+++ app/src/main/java/com/shayekh/daggerimpl/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private Car car;
+
+    @Inject Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CarComponent component = DaggerCarComponent.create();
+        component.inject(this);
+
+//        car =  component.getCar();
+        car.drive();
     }
 }
